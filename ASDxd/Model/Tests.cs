1ae1f480650456235fa26d4ccab248e9ceda2c97
Patrick Espiritu <62047924+patrick-espiritu@users.noreply.github.com>
2022-09-18 22:50:44 +1000

    Add nunit to project

---
+++ ASDxd/Model/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+
+namespace ASDxd.Model
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
