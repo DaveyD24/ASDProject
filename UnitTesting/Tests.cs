701cf6336554424bbe118e8e1aba5092d8d1d5d9
DaveyD24 <69884791+DaveyD24@users.noreply.github.com>
2022-10-06 17:06:46 +1100

    NUnit Setup Again

---
+++ UnitTesting/Tests.cs
@@ -0,0 +1,37 @@
+﻿using ASDNew.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+
+        //Patrick.E
+        //F109: Adding products to restaurant
+        [Test]
+        public void TestProduct()
+        {
+            //Product p = new Product();
+            //Assert.That(p.Add(5, 3), Is.EqualTo(8), "Sum of the two numbers does not match expected answer");
+
+            Product p = new Product
+            {
+                Name = "Test",
+            };
+            Assert.AreEqual("Test", p.Name);
+        }
+    }
+}
