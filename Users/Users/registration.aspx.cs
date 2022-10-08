830fab67ed9ce674948d590e78c7268127867c95
Jaghadishan <jaghadishan@gmail.com>
2022-10-08 17:34:29 +1100

    Login and registration page
    Login and registration page without CSS

---
+++ Users/Users/registration.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Ajax.Utilities;
+
+namespace Users
+{
+    public partial class registration : System.Web.UI.Page
+    {
+        string connectionString = @"Data Source=MSI\SQLEXPRESS;Initial Catalog=UserRegistrationDB;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                Clear();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "" || txtPassword.Text == "")
+                lblErrorMessage.Text = "Please fill out the mandatory fields";
+            else if (txtPassword.Text != txtConfirmPassword.Text)
+                lblErrorMessage.Text = "Passwords don't match";
+            else
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand sqlCmd = new SqlCommand("UserAddOrEdit", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@UserID", Convert.ToInt32(hfUserID.Value == "" ? "0" : hfUserID.Value));
+                    sqlCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@LastName", txtLastName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Contact", txtContact.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+                    sqlCmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    Clear();
+                    lblSuccessMessage.Text = "Submitted Successfully";
+                }
+            }
+        }
+
+        void Clear()
+        {
+            txtFirstName.Text = txtLastName.Text = txtContact.Text = txtAddress.Text = txtUsername.Text = txtPassword.Text = txtConfirmPassword.Text = "";
+            hfUserID.Value = "";
+            lblSuccessMessage.Text = lblErrorMessage.Text = "";
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
