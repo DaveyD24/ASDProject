0f6f0d891243cb4283169929ef86547cf2be2241
abishek123 <abishek.sundar@gmail.com>
2022-10-19 11:14:51 +0530

    OrderDetails.CS
    OrderDetails model file

---
+++ ASDNew/Models/OrderDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping.Models
+{
+    public class OrderDetails
+    {
+        public int Sno { get; set; }
+        public string OrderID { get; set; }
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Price { get; set; }
+        public string Quantity { get; set; }
+        public string DateOfOrder { get; set; }
+        public string Email { get; set; }
+
+    }
+}
